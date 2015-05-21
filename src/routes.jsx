2098e784b4485fd5757aa244ab2b00d1d35a269b
Ernie Casilla <ecasilla@icloud.com>
2015-05-20 20:11:28 -0400

    fixed login added about page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import App from './App';
 import {Route, NotFoundRoute} from 'react-router';
-import Home from './components/Home';
+import Home from './components/Home/Home';
+import About from './components/Home/About';
 import NotFound from './components/not-found';
 import Login from './components/Sessions/Login';
 import Signup from './components/Sessions/Signup';
