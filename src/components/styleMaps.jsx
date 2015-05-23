6cdfea9fcbd6a99b52142c10013bb2121af02a20
%%GITNAME%% <%%GITEMAIL%%>
2015-05-23 18:10:30 -0400

    adding modernizr bs and some preloader ui components

---
+++ src/components/styleMaps.jsx
@@ -0,0 +1,10 @@
+const styleMaps = {
+    SIZES: {
+        'large': 'lg',
+        'medium': 'md',
+        'small': 'sm',
+        'xsmall': 'xs'
+    }
+};
+
+export default styleMaps;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import React from 'react';
 import Router from 'react-router';
 import routes from './routes';
 import RouterContainer from './services/RouterContainer';
+import Modernizr from './assets/vendor/modernizr';
+import './components/preloader.jsx';
+import Pace from './assets/vendor/pace';
 
 RouterContainer.set(Router);
 RouterContainer.set(Router.create({ routes }));
-
-Router.run(routes,function (Handler) {
-  React.render(<Handler/>, document.getElementById('app'));
+Pace.once('hide', function() {
+  $('#pace-loader').removeClass('pace-big').addClass('pace-small');
 });
+
+function RouterInit(Handler){
+  if(window.hasOwnProperty('ga') && typeof window.ga === 'function') {
+    window.ga('send', 'pageview', {
+      'page': window.location.pathname + window.location.search  + window.location.hash
+    });
+  }
+  Pace.restart();
+  React.render(<Handler/>, document.getElementById('app'));
+}
+
+Router.run(routes, RouterInit);
+
+//if(Modernizr.history){
+  //Router.run(routes, ReactRouter.HistoryLocation, RouterInit);
+//}else{
+  //Router.run(routes, RouterInit);
+//}
+
