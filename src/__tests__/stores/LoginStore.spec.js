d3ee1ea37c97daa99c8abd5b7bb03a4897f51075
Ernie Casilla <ecasilla@icloud.com>
2015-07-15 13:51:27 -0400

    fixing a unit test to represent the actual action payload

--- src/__tests__/stores/LoginStore.spec.js
+++ src/__tests__/stores/LoginStore.spec.js
@@ -16,7 +16,7 @@ describe('LoginStore: ', () => {
     expect(store).to.be.an('object');
   });
   it('should not have a user defined initially',() => {
-   expect(store._user).to.be.null;
+   expect(store._user).to.equal(null);
   });
   it('should inherit a subscribe method',() => {
    expect(store.subscribe).to.be.an('function');
@@ -30,17 +30,24 @@ describe('LoginStore: ', () => {
     expect(store.isLoggedIn()).to.equal(true);
   });
   it('should set the user when a Login action is recevied',() => {
-   var testAction = {actionType:CONSTANTS.ActionTypes.LOGIN,user:{name:'subject'}};
-   var emitSpy = sandbox.spy(store,'emit');
+   var testAction = {action:{actionType:CONSTANTS.ActionTypes.LOGIN,user:{name:'subject'}}};
+   var emitSpy = sandbox.spy(store,'emitChange');
+   store._registerToActions(testAction);
+   expect(store.user.name).to.be.equal('subject');
+   expect(emitSpy.called).to.be.equal(true);
+  });
+  it('should set the user when a Signup action is recevied',() => {
+   var testAction = {action:{actionType:CONSTANTS.ActionTypes.SIGN_UP,user:{name:'subject'}}};
+   var emitSpy = sandbox.spy(store,'emitChange');
    store._registerToActions(testAction);
    expect(store.user.name).to.be.equal('subject');
    expect(emitSpy.called).to.be.equal(true);
   });
   it('should remove the user when a Logout action is recevied',() => {
-   var testAction = {actionType:CONSTANTS.ActionTypes.LOGOUT};
-   var emitSpy = sandbox.spy(store,'emit');
+   var testAction = {action:{actionType:CONSTANTS.ActionTypes.LOGOUT,user:{name:'subject'}}};
+   var emitSpy = sandbox.spy(store,'emitChange');
    store._registerToActions(testAction);
-   expect(store.isLoggedIn()).to.equal(false);
    expect(emitSpy.called).to.be.equal(true);
+   expect(store._user).to.equal(null);
   });
 });
